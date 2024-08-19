05ec6fc37836841935494041c4220d36ab09e881
mmtrucefacts <93686164+mmtrucefacts@users.noreply.github.com>
2024-08-19 17:21:00 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+// src/index.js
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// static
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/service/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
