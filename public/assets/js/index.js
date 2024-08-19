f82f88de6f6d1adbf723a8d1425e4e9897a9aa8e
mmtrucefacts <93686164+mmtrucefacts@users.noreply.github.com>
2024-08-19 17:30:52 -0500

    Update index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,8 @@
-document.getElementById('proxy-form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            const url = document.getElementById('url-input').value;
-            window.location.href = '/gt/' + __uv$config.encodeUrl(url);
-        });
+function encodeUrl(url) {
+          return btoa(url); // encode
+      }
+       document.getElementById('proxy-form').addEventListener('submit', function(event) {
+          event.preventDefault();
+           const url = document.getElementById('url-input').value;
+           const encodedUrl = encodeUrl(url);            window.location.href = '/gt/' + encodedUrl;      
+       });
