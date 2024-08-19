283af50a9310d7e5a1585eadc2c024c8380b1b49
mmtrucefacts <93686164+mmtrucefacts@users.noreply.github.com>
2024-08-19 17:17:15 -0500

    Update index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,5 @@
 document.getElementById('proxy-form').addEventListener('submit', function(event) {
             event.preventDefault();
             const url = document.getElementById('url-input').value;
-            window.location.href = '/service/' + __uv$config.encodeUrl(url);
+            window.location.href = '/gt/' + __uv$config.encodeUrl(url);
         });
